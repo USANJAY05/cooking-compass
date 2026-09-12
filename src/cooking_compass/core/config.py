b37a9cd12c2a5d089ee1292fc27010eb17d5008e
SANJAY U <115535475+USANJAY05@users.noreply.github.com>
2026-09-12 07:22:34 +0530

    refactor: derive B2 endpoint from region

--- src/cooking_compass/core/config.py
+++ src/cooking_compass/core/config.py
@@ -7,10 +7,7 @@ load_dotenv()
 
 # Backblaze B2 S3-compatible storage
 B2_REGION = os.getenv("B2_REGION", "us-east-005")
-B2_ENDPOINT = os.getenv(
-    "B2_ENDPOINT",
-    f"https://s3.{B2_REGION}.backblazeb2.com",
-)
+B2_ENDPOINT = f"https://s3.{B2_REGION}.backblazeb2.com"
 B2_BUCKET = os.getenv("B2_BUCKET")
 B2_KEY_ID = os.getenv("B2_KEY_ID")
 B2_APPLICATION_KEY = os.getenv("B2_APPLICATION_KEY")
